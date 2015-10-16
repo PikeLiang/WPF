9bf3dd580db2f7af522f41082b66792447ed5df8
Ting <356571795@qq.com>
2015-10-17 00:04:24 +0800

    1.10WPF动画-书写文字

---
+++ 1.10WPF动画-书写文字/Wpfdonghua2/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpfdonghua2
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+
+		}
+
+		private void ExitRectangle_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
+}
